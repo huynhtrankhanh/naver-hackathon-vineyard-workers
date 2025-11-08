6be49449c7b77a2b6bd31c8953e08a6576bc9302
Chien Thang <thangprovb@gmail.com>
2025-11-08 15:42:25 +0700

    delete old code

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-// .prettierrc.js
-module.exports = {
-  semi: false, // Quy tắc 'semi'(không dùng ; )
-  singleQuote: true, // Quy tắc 'quotes' (dùng ' thay vì ")
-  tabWidth: 2, // Quy tắc 'indent' (2 cách)
-  bracketSpacing: true, // Quy tắc 'object-curly-spacing' 
-  trailingComma: 'es5', // Quy tắc 'comma-dangle'
-  arrowParens: 'always',
-}
